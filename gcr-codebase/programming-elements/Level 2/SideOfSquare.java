0fd7c096b69a3b8974c1131a097242c711e46dc4
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2025-12-17 16:38:47 +0530

    [Nageshwar] : added Square side calculation from its perimeter

---
+++ gcr-codebase/programming-elements/Level 2/SideOfSquare.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class SideOfSquare{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter perimeter of square: ");
+		float peri = sc.nextFloat();
+		float side = peri/4;
+		
+		System.out.println("The length of the side is "+side+" whose perimeter is "+peri);
+	}
+}
