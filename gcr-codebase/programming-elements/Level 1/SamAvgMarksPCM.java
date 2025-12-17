df57cfef4101b3b8d4de44a147f559d1912867d1
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2025-12-17 12:42:38 +0530

    [Nageshwar] : added Sam's percentage average marks in PCM

---
+++ gcr-codebase/programming-elements/Level 1/SamAvgMarksPCM.java
@@ -0,0 +1,10 @@
+public class SamAvgMarksPCM{
+	public static void main(String args[]){
+		int mathMarks = 94;
+		int physicsMarks = 95;
+		int chemistryMarks = 95;
+		int total = mathMarks+physicsMarks+chemistryMarks;
+		double samPercentage = total/3.0;
+		System.out.println("Sam's average mark in PCM is "+samPercentage);
+	}
+}
