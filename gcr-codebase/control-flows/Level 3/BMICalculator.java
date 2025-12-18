50cdb49012d3469c9c6ada2259aaeb33eec27c39
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2025-12-18 18:36:21 +0530

    [Nageshwar Added BMI calculator

---
+++ gcr-codebase/control-flows/Level 3/BMICalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BMICalculator{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		double weight = sc.nextDouble();
+		double height = sc.nextDouble();
+		
+		height= height/100;
+		double bmi = weight/(height*height);
+		if(bmi>=40)
+			System.out.println("Obese");
+		else if(bmi>=25)
+			System.out.println("Overweight");
+		else if(bmi>18.5)
+			System.out.println("Normal");
+		else
+			System.out.println("Underweight");
+	}
+}
