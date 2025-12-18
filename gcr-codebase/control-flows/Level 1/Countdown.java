91fe808cfcbf8dd3bbcad75b1f7c7464fe64ef96
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2025-12-18 12:59:35 +0530

    [Nageshwar] : added Countdown using for loop

---
+++ gcr-codebase/control-flows/Level 1/Countdown.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Countdown{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int counter = sc.nextInt();
+		for(int i=counter;i>0;i--)
+			System.out.println(i);
+	}
+}
