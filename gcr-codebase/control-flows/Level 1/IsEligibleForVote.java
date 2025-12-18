9b73def2d5d250800e06f2d63b0b88686ff94f4e
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2025-12-18 12:25:51 +0530

    [Nageshwar] :added Voting eligibility

---
+++ gcr-codebase/control-flows/Level 1/IsEligibleForVote.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class IsEligibleForVote{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		if(age>=18)
+			System.out.println("The person can vote");
+		else
+			System.out.println("The person cannot vote");
+	}
+}
