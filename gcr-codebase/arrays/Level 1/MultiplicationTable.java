0668461ec2dc7b1ea07f010f9c5e5b239435db7f
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2025-12-19 12:10:31 +0530

    [Nageshwar] : added Multiplication of number from 1 to 10

---
+++ gcr-codebase/arrays/Level 1/MultiplicationTable.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MultiplicationTable{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		int arr[] = new int[10];
+		for(int i=1;i<=10;i++)
+			arr[i-1] = num*i;
+		
+		for(int i=1;i<=10;i++)
+			System.out.println(num+" * "+ i+" = "+arr[i-1]);
+	}
+}
