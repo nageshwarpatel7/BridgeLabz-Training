f817fc2664a7376cc27212826c893d82464f3994
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2025-12-20 12:36:04 +0530

    [Nageshwar] : added Largest and Smallest among three numbers logic

--- gcr-codebase/methods/LargestNumber.java
+++ gcr-codebase/methods/LargestNumber.java
@@ -10,16 +10,26 @@ public class LargestNumber{
 		System.out.println("Enter third number: ");
 		int num3 = sc.nextInt();
 		
-		largestNum(num1, num2, num3);
+		int ans [] =largestNum(num1, num2, num3);
+		System.out.println("Smallest number is: "+ans[0]);
+		System.out.println("Largest number is: "+ans[1]);
 	}
-	public static void largestNum(int num1, int num2, int num3){
+	public static int[] largestNum(int num1, int num2, int num3){
+		int ans[] = new int[2];
 		if(num1>num2 && num1>num3)
-			System.out.println("First number is largest");
+			ans[1]=num1;
 		else if(num2>num1 && num2>num3)
-			System.out.println("Second number is largest");
-		else if(num3>num2 && num3>num1)
-			System.out.println("Third number is largest");
+			ans[1] = num2;
 		else
-			System.out.println("There is no single large number");
+			ans[1] =num3;
+		
+		if(num1<num2 && num1<num3)
+			ans[0] = num1;
+		else if(num2<num1 && num2<num3)
+			ans[0] = num2;
+		else
+			ans[0] = num3;
+		
+		return ans;
 	}
 }
