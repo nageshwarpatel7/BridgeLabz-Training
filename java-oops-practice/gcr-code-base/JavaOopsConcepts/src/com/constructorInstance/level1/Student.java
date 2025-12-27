fb507c8046f278c66cc900701871f6ba2109a773
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2025-12-27 16:39:36 +0530

    [Nageshwar] : added student marks display logic

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/constructorInstance/level1/Student.java
@@ -0,0 +1,19 @@
+package com.constructorInstance.level1;
+
+public class Student {
+	public int rollNumber;
+	protected String name;
+	private double cgpa;
+	
+	Student(int rollNumber, String name, double cgpa){
+		this.rollNumber = rollNumber;
+		this.name = name;
+		this.cgpa = cgpa;
+	}
+	public void updateCGPA(double cg) {
+		this.cgpa = cg;
+	}
+	public void displayCGPA() {
+		System.out.println("CGPA: "+this.cgpa);
+	}
+}
