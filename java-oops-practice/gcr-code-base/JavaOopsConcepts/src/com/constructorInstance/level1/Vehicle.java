28fab756df018250e209c8579d41016c42d37755
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2025-12-27 16:15:22 +0530

    [Nageshwar] : added vehicle registration logic

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/constructorInstance/level1/Vehicle.java
@@ -0,0 +1,30 @@
+package com.constructorInstance.level1;
+
+public class Vehicle {
+	public String ownerName;
+	public String vehicleType;
+	public static int registrationFee =500;
+	
+	public Vehicle(String ownerName, String vehicleType) {
+		this.ownerName = ownerName;
+		this.vehicleType = vehicleType;
+	}
+	public void displayVehicleDetails() {
+		System.out.println("Owner name: "+this.ownerName);
+		System.out.println("Vehicle type: "+this.vehicleType);
+		System.out.println("Registration fee: "+this.registrationFee+"\n");
+	}
+	
+	public static void main(String[] args) {
+		Vehicle v1 = new Vehicle("TVS raider", "Two-whiller");
+		v1.displayVehicleDetails();
+		
+		Vehicle.updateRegistrationFee(600);
+		
+		Vehicle v2 = new Vehicle("Scorpio","Four-whiller");
+		v2.displayVehicleDetails();
+	}
+	public static void updateRegistrationFee(int fee) {
+		Vehicle.registrationFee = fee;
+	}
+}
