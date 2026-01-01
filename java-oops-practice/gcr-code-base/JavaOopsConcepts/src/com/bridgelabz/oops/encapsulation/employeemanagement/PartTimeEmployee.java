c79e61f7276c04a8baedc998e02b5b84e8c13e2f
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-01 22:24:15 +0530

    [Nageshwar] : added Employee management system encapsulation logic

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/employeemanagement/PartTimeEmployee.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.oops.encapsulation.employeemanagement;
+
+public class PartTimeEmployee extends Employee{
+	private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(String id, String name, int hours, double rate) {
+        super(id, name, 0); 
+        this.hoursWorked = hours;
+        this.hourlyRate = rate;
+    }
+    
+    @Override
+    public double calculateSalary() {
+    	return hoursWorked* hourlyRate;
+    }
+}
