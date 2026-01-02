aa72fb5f7d46dff29ac148c4145f2462272ae789
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-02 17:20:36 +0530

    [Nageshwar] : added banking system encapsulation logic

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/bankingsystem/BankingSystemDemo.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.oops.encapsulation.bankingsystem;
+
+public class BankingSystemDemo {
+	public static void main(String[] args) {
+        
+        BankAccount saving = new SavingsAccount("SA123", "Nageshwar", 6000.0);
+        BankAccount current = new CurrentAccount("CA456", "Himesh", 2000.0);
+
+        BankAccount[] bankDB = { saving, current };
+
+        System.out.println("--- Monthly Account Processing ---");
+        for (BankAccount acc : bankDB) {
+            acc.calculateInterest();
+            System.out.println(acc.getHolderName() + " Balance: $" + acc.getBalance());
+            System.out.println("-------------------------");
+        }
+
+        System.out.println("--- Loan Application Process ---");
+        if (saving instanceof Loanable) {
+            ((Loanable) saving).applyForLoan(10000);
+        }
+    }
+}
