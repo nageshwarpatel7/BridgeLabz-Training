6f8b75714a4458db1c390c97be74a929ccbedcf6
Nageshwar Prashad Patel <119944281+nageshwarpatel7@users.noreply.github.com>
2026-01-05 14:43:25 +0530

    [Nageshwar] : added HashMap implementation logic

--- dsa-practice/gcr-code-base/DataStructures/src/hashmap/Node.java
+++ dsa-practice/gcr-code-base/DataStructures/src/hashmap/Node.java
@@ -1,5 +1,13 @@
 package hashmap;
 
 public class Node {
+  int key;
+    int val;
+    Node next;
 
+    Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+        this.next = null;
+    }
 }
