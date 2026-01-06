32813c189c9a282eb5f7e0b5d373e145b414fc77
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-06 11:36:35 +0530

    [Nageshwar] : added counting sort logic

--- dsa-practice/gcr-code-base/DataStructures/src/sorting/CountingSort.java
+++ dsa-practice/gcr-code-base/DataStructures/src/sorting/CountingSort.java
@@ -6,8 +6,10 @@ public class CountingSort {
         int max = arr[0];
         int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) max = arr[i];
-            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max)
+            	max = arr[i];
+            if (arr[i] < min) 
+            	min = arr[i];
         }
         int range = max - min + 1;
         int[] count = new int[range];
