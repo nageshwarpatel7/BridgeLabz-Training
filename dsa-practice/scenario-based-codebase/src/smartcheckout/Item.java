161ac4c99c2bd7d22913575f748befe9f85ee445
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-15 11:38:14 +0530

    [Nageshwar] : added item file

---
+++ dsa-practice/scenario-based-codebase/src/smartcheckout/Item.java
@@ -0,0 +1,11 @@
+package smartcheckout;
+
+public class Item {
+    String name;
+    int quantity;
+
+    Item(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
