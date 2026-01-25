dcafb8c0bb6967956b584002a26e893a5e2cb54d
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-25 12:28:58 +0530

    [Nageshwar] : added convert all uppercase letter to lowercase in file

--- java-collections-practice/gcr-code-codebase/collectionsPractice/src/JavaStreams/ConvertUpperToLower.java
+++ java-collections-practice/gcr-code-codebase/collectionsPractice/src/JavaStreams/ConvertUpperToLower.java
@@ -10,7 +10,7 @@ public class ConvertUpperToLower {
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new FileReader("file.txt"));
-		BufferedWriter bw = new BufferedWriter(new FileWriter("Upper.txt"));
+		BufferedWriter bw = new BufferedWriter(new FileWriter("Lower.txt"));
 		
 		String line;
 		
