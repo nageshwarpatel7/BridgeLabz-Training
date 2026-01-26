0b0a86e2ab09d0c058ddf1448f4b9cdfedeceaff
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-26 21:35:53 +0530

    [Nageshwar] : added Testing Banking Transactions logic

---
+++ java-collections-practice/gcr-code-codebase/collectionsPractice/src/junit/AdvancedJunitTesting/InsuffiecientBalanceException.java
@@ -0,0 +1,8 @@
+package junit.AdvancedJunitTesting;
+
+public class InsuffiecientBalanceException extends Exception{
+	
+	public InsuffiecientBalanceException(String message) {
+		super(message);
+	}
+}
