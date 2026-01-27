d07d49e4574956f4e2cca771a5beae861418ba09
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-27 14:31:40 +0530

    [Nageshwar] : added sort csv records by salary

---
+++ java-io-streams-practice/gcr-code-codebase/iostreamspractice/src/iostreamspractice/csv/Intermediateproblem/Employee.java
@@ -0,0 +1,18 @@
+package iostreamspractice.csv.Intermediateproblem;
+
+class Employee {
+    String name;
+    String department;
+    double salary;
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | " + department + " | " + salary;
+    }
+}
