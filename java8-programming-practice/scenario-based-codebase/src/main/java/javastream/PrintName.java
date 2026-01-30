9267aa68a4445233e2edb0cc3f9cca2d06b9a21c
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-30 22:35:43 +0530

    [Nageshwar] : added Print the names of all students only

---
+++ java8-programming-practice/scenario-based-codebase/src/main/java/javastream/PrintName.java
@@ -0,0 +1,15 @@
+package javastream;
+import java.util.*;
+
+public class PrintName {
+	public static void main(String[] args) {
+		Student[] student = { new Student("Nageshwar Prashad Patel",111,"Nageshwar",22,"M","Katni",1,"7447878384"),
+				   new Student("Lucky Pal",98,"Lucky",22,"M","Bhopal",2,"8378492379"),
+				   new Student("Himesh Kurmi",83,"Himesh",21,"M","Sagar",3,"6524783574"),
+				   new Student("xysd iad",78,"xysd",26,"F","hfjksh",53,"6724893471"),
+				   new Student("jhsjfy hjs", 67,"jhsjfy",38,"F","hskf",49,"7346274823")};
+		
+		Arrays.stream(student).map(stu->stu.name).forEach(System.out::println);
+		
+	}
+}
