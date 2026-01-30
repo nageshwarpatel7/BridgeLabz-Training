91e5f461cc52ad9a16c7106091c18a86bb841166
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-30 22:16:57 +0530

    [Nageshwar] : added Print all students whose age is greater than 25

--- java8-programming-practice/scenario-based-codebase/src/main/java/javastream/AgeComparision.java
+++ java8-programming-practice/scenario-based-codebase/src/main/java/javastream/AgeComparision.java
@@ -2,14 +2,18 @@ package javastream;
 import java.util.*;
 
 public class AgeComparision {
-	public static void ageGreaterThan25(int[] arr) {
+	public static void ageGreaterThan25(Student[] arr) {
 		
-		Arrays.stream(arr).filter(age -> age>25).forEach(System.out::println);
+		Arrays.stream(arr).filter(student -> student.age>25).forEach(System.out::println);
 	}
 	
 	public static void main(String[] args) {
 		
-		int[] ages = {17, 19,25, 26, 29, 39,40};
+		Student[] ages = { new Student("Nageshwar Prashad Patel",111,"Nageshwar",22,"M","Katni",1,"7447878384"),
+						   new Student("Lucky Pal",98,"Lucky",22,"M","Bhopal",2,"8378492379"),
+						   new Student("Himesh Kurmi",83,"Himesh",21,"M","Sagar",3,"6524783574"),
+						   new Student("xysd iad",78,"xysd",26,"F","hfjksh",53,"6724893471"),
+						   new Student("jhsjfy hjs", 67,"jhsjfy",38,"F","hskf",49,"7346274823")};
 		
 		ageGreaterThan25(ages);
 	}
