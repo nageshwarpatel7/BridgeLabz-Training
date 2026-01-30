704caf16bc48e1c137fca603da8a02f0b7a75eca
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-30 16:18:00 +0530

    [Nageshwar] : smart vehicle dashboard login

---
+++ java8-programming-practice/gcr-codebase/src/main/java/functionalInterface/defaultinterfacemethod/smartvehicledashboard/VehicleDemo.java
@@ -0,0 +1,13 @@
+package functionalInterface.defaultinterfacemethod.smartvehicledashboard;
+
+public class VehicleDemo {
+	public static void main(String[] args) {
+		BMW bmw = new BMW();
+		
+		bmw.displaySpeed();
+		
+		Ola ola = new Ola();
+		ola.displaySpeed();
+		ola.displayBattery(80);
+	}
+}
