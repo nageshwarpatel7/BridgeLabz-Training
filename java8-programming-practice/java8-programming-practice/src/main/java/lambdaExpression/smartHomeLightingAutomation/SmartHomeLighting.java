018109fb1ef54a384205f6ff80bdca717781c3f3
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-30 09:55:38 +0530

    [Nageshwar] : added smart home lighting automation

--- java8-programming-practice/java8-programming-practice/src/main/java/lambdaExpression/smartHomeLightingAutomation/SmartHomeLighting.java
+++ java8-programming-practice/java8-programming-practice/src/main/java/lambdaExpression/smartHomeLightingAutomation/SmartHomeLighting.java
@@ -7,13 +7,13 @@ public class SmartHomeLighting {
         SmartLightController controller = new SmartLightController();
 
         LightAction motionTrigger = () ->
-                System.out.println("💡 Lights ON at full brightness (Motion detected)");
+                System.out.println("Lights ON at full brightness (Motion detected)");
 
         LightAction eveningTrigger = () ->
-                System.out.println("🌙 Warm lights ON at 40% brightness");
+                System.out.println("Warm lights ON at 40% brightness");
 
         LightAction voiceTrigger = () ->
-                System.out.println("🎙️ Voice command: Party mode activated 🎉");
+                System.out.println("Voice command: Party mode activated 🎉");
 
         controller.execute(motionTrigger);
         controller.execute(eveningTrigger);
