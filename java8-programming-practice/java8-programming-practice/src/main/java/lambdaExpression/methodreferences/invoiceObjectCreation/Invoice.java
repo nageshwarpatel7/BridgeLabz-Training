d4da382041ae89378bdf92d77b7614c3eb333d2a
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-01-30 10:11:51 +0530

    [Nageshwar] : added Invoice Object Creation logic

---
+++ java8-programming-practice/java8-programming-practice/src/main/java/lambdaExpression/methodreferences/invoiceObjectCreation/Invoice.java
@@ -0,0 +1,14 @@
+package lambdaExpression.methodreferences.invoiceObjectCreation;
+
+public class Invoice {
+	private int transactionId;
+
+    public Invoice(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{transactionId=" + transactionId + "}";
+    }
+}
