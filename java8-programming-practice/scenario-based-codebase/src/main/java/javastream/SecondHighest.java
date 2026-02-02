2f5760ce1489fe4bfb0ba00d97a03173d80fdcc7
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-02-02 11:56:02 +0530

    [Nageshwar] : added Find the second best rank holder

---
+++ java8-programming-practice/scenario-based-codebase/src/main/java/javastream/SecondHighest.java
@@ -0,0 +1,17 @@
+package javastream;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SecondHighest {
+	public static void main(String[] args) {
+		List<Student> student = List.of(new Student("Nageshwar Prashad Patel",111,"Nageshwar",22,"M","Katni",1,"7447878384"),
+				   new Student("Lucky Pal",98,"Lucky",22,"M","Bhopal",2,"8378492379"),
+				   new Student("Himesh Kurmi",83,"Himesh",21,"M","Sagar",3,"6524783574"),
+				   new Student("xysd iad",78,"xysd",26,"F","Mumbai",53,"6724893471"),
+				   new Student("jhsjfy hjs", 67,"jhsjfy",38,"F","hskf",49,"7346274823"));
+		
+		student.stream().sorted(Comparator.comparingInt(s->s.rank)).skip(1).findFirst()
+		.ifPresent(sec -> System.out.println("Student with second rank: "+sec));
+	}
+}
