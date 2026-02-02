699664254d27c843a59e40dc61cb2018936e3dd3
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-02-02 12:09:07 +0530

    [Nageshwar] : added Find the oldest student

---
+++ java8-programming-practice/scenario-based-codebase/src/main/java/javastream/OldestStudent.java
@@ -0,0 +1,15 @@
+package javastream;
+import java.util.*;
+
+public class OldestStudent {
+	public static void main(String[] args) {
+		List<Student> student = List.of(new Student("Nageshwar Prashad Patel",111,"Nageshwar",22,"M","Katni",1,"7447878384"),
+				   new Student("Lucky Pal",98,"Lucky",22,"M","Bhopal",2,"8378492379"),
+				   new Student("Himesh Kurmi",83,"Himesh",21,"M","Sagar",3,"6524783574"),
+				   new Student("xysd iad",78,"xysd",26,"F","Mumbai",53,"6724893471"),
+				   new Student("jhsjfy hjs", 67,"jhsjfy",38,"F","hskf",49,"7346274823"));
+		
+		student.stream().max(Comparator.comparingInt(s->s.age))
+		.ifPresent(s-> System.out.println("Oldest student: "+s));
+	}
+}
