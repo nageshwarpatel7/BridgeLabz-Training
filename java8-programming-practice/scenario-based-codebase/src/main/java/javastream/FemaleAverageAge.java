1b5fa5a3bff4db2ded7aa8a912a85544860bb55b
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-02-04 13:24:51 +0530

    [Nageshwar] : added Find the average age of male students

---
+++ java8-programming-practice/scenario-based-codebase/src/main/java/javastream/FemaleAverageAge.java
@@ -0,0 +1,21 @@
+package javastream;
+
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class FemaleAverageAge {
+	public static void main(String[] args) {
+		List<StudentInfo> students = List.of(
+                new StudentInfo("Nageshwar Prashad Patel", 111, "Nageshwar", 22, "M", "Katni", 1, List.of("7447878384", "7447878385")),
+                new StudentInfo("Lucky Pal", 98, "Lucky", 22, "M", "Bhopal", 2, List.of("8378492379")),
+                new StudentInfo("Himesh Kurmi", 83, "Himesh", 20, "M", "Sagar", 3, List.of("6524783574", "6524783575")),
+                new StudentInfo("xysd iad", 78, "xysd", 26, "F", "Mumbai", 53, List.of("6724893471")),
+                new StudentInfo("jhsjfy hjs", 67, "jhsjfy", 38, "F", "hskf", 49, List.of("7346274823", "7346274824"))
+        );
+		
+		OptionalDouble avgAge =students.stream()
+		.filter(s->s.gender.equalsIgnoreCase("M")).mapToInt(s->s.age).average();
+		
+		avgAge.ifPresent(age->System.out.println("Average age of female students: "+age));
+	}
+}
