1476f4db8b4205c62852a0d7fd619ce26f145f16
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-02-04 12:32:35 +0530

    [Nageshwar] : added Check if no student belongs to Bhopal

---
+++ java8-programming-practice/scenario-based-codebase/src/main/java/javastream/StudentNotBelongToBhopal.java
@@ -0,0 +1,18 @@
+package javastream;
+
+import java.util.List;
+
+public class StudentNotBelongToBhopal {
+	public static void main(String[] args) {
+		List<StudentInfo> students = List.of(
+                new StudentInfo("Nageshwar Prashad Patel", 111, "Nageshwar", 22, "M", "Katni", 1, List.of("7447878384", "7447878385")),
+                new StudentInfo("Lucky Pal", 98, "Lucky", 22, "M", "Bhopal", 2, List.of("8378492379")),
+                new StudentInfo("Himesh Kurmi", 83, "Himesh", 20, "M", "Sagar", 3, List.of("6524783574", "6524783575")),
+                new StudentInfo("xysd iad", 78, "xysd", 26, "F", "Mumbai", 53, List.of("6724893471")),
+                new StudentInfo("jhsjfy hjs", 67, "jhsjfy", 38, "F", "hskf", 49, List.of("7346274823", "7346274824"))
+        );
+		
+		boolean ans = students.stream().noneMatch(s->s.city.equalsIgnoreCase("Bhopal"));
+		System.out.println("No student belongs to: "+ans);
+	}
+}
